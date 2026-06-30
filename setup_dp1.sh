82d75097d8abecf881170d523513710f006d27a5
patricialarsen <tricia340@gmail.com>
2026-06-30 11:36:10 -0500

    Create setup_dp1.sh

---
+++ setup_dp1.sh
@@ -0,0 +1,12 @@
+source /cvmfs/sw.lsst.eu/almalinux-x86_64/lsst_distrib/v29.2.1/loadLSST-ext.bash
+setup lsst_distrib
+
+export REPO=/global/cfs/cdirs/lsst/production/gen3/rubin/DP1/repo/butler.yaml
+
+setup -r ./bps_parsl_sites -j
+setup -r ./source_injection_pl -j
+setup -r ./pipe_tasks_pl -j
+setup -r ./obs_base -j
+setup -r ./analysis_tools -j
+
+eups list 
